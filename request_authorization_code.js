768e37d89814becca197eafa187cecde06ff6d87
Wallacy V. da Silva <wlcsv8080@outlook.com>
2023-12-22 23:29:52 -0300

    Por que a parte de autenticação é sempre a mais complicada?

--- request_authorization_code.js
+++ request_authorization_code.js
@@ -5,7 +5,11 @@ const querystring = require('querystring')
    var state = generateRandomString(16)
    var scope = 'user-read-private user-read-email'
 
-   const login = "https://accounts.spotify.com/authorize?" + querystring.stringify(
-
-   )
+   const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
+     response_type: 'code',
+     client_id: client_id,
+     scope: scope,
+     redirect_uri: redirect_uri,
+     state: state
+   })
  }
