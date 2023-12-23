334048c57cad7253a8cbba722f598e4fafea46c8
Wallacy V. da Silva <wlcsv8080@outlook.com>
2023-12-23 01:21:01 -0300

    Criação da função generateRandomString

--- request_authorization_code.js
+++ request_authorization_code.js
@@ -1,15 +1,27 @@
 const axios = require('axios')
 const querystring = require('querystring')
 
- async function request_authorization(client_id) {
-   var state = generateRandomString(16)
-   var scope = 'user-read-private user-read-email'
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
 
-   const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
-     response_type: 'code',
-     client_id: client_id,
-     scope: scope,
-     redirect_uri: redirect_uri,
-     state: state
-   })
- }
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    result += characters.charAt(randomIndex)
+  }
+
+  return result
+
+}
+
+async function request_authorization(client_id) {
+  var state = generateRandomString(16)
+  var scope = 'user-read-private user-read-email'
+  const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
+    response_type: 'code',
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state
+  })
+}
