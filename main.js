b3429b9bf543564de5df091f7460cea9c6b447d3
Wallacy V. da Silva <wlcsv8080@outlook.com>
2023-12-24 14:23:40 -0300

    Se isso rodar de primeira eu vou ficar muito impressionado

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const request_token = require('./request_token')
 const request_artist_data = require('./request_artist_data')
 const request_authorization = require('./request_authorization_code')
+
+const express = require('express')
+
 require('dotenv').config()
 
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
 const port = process.env.PORT
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Não tem nada para você aqui')
+})
+
+app.get('/callback', (req, res) => {})
 
 let artist_id = '4Z8W4fKeB5YxbusRsdQVPb'
 
@@ -13,6 +24,11 @@ async function main() {
     const token = await request_token.token(client_id, client_secret)
     // console.log(token)
     await request_artist_data.artist_data(artist_id, token)
+    await request_authorization.request_authorization(client_id)
 }
 
 main()
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
--- request_authorization_code.js
+++ request_authorization_code.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
-const express = require('express')
 const querystring = require('querystring')
 
-exports.request_authorization = async function request_authorization(client_id, port) {
-  const app = express()
-
+exports.request_authorization = async function request_authorization(client_id, client_secret) {
   var state = generateRandomString(16)
   var scope = 'user-read-private user-read-email'
+
+  const redirect_uri = 'http://localhost:8888/callback'
+
   const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
     response_type: 'code',
     client_id: client_id,
@@ -14,34 +14,56 @@ exports.request_authorization = async function request_authorization(client_id, 
     redirect_uri: redirect_uri,
     state: state
   })
+
+  console.log(login)
   
-  
-  app.get('/', (req, res) => {
-    res.send('Não tem nada para você aqui')
-  })
-  
-  let code 
-  let state
-  
-  
-  app.get('/callback', (req, res) => {})
+  let code, state_returned   
   
   await axios.get('http://localhost:8888/callback')
-    .then(reponse => {
+    .then(response => {
       console.log("Response:", response)
       code = response.code
-      state = response.state
+      state_returned = response.state
     })
     .catch(error => {
       console.log('Error in request:', error)
     })
 
-  app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
-  })
+    if (state == state_returned) {
+      console.log('Equal')
+    } else {
+      return
+    }
+
+    const data = {
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirect_uri
+    }
+
+    const config = {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      }
+    }
+
+    function Response(access_token = undefined, token_type = undefined, scope = undefined, expires_in = undefined, refresh_token = undefined) {
+        this.access_token = access_token
+        this.token_type = token_type
+        this.scope = scope
+        this.expires_in = expires_in
+        this.refresh_token = refresh_token
+    }
+
+    let response = new Response()
+
+    await axios.post('https://accounts.spotify.com/api/token', data, config)
+      .then(response => {
+        response.access_token = response.access_token
+      })
+
 }
-
-
 
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -53,5 +75,4 @@ function generateRandomString(length) {
   }
 
   return result
-
 }
