862e5d8b155faae38bd21d7e011ad68a0354844f
Wallacy V. da Silva <wlcsv8080@outlook.com>
2023-12-24 00:10:01 -0300

    Olha o código do express devia e vai ir no main hein, só te avisando

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const request_token = require('./request_token')
 const request_artist_data = require('./request_artist_data')
+const request_authorization = require('./request_authorization_code')
 require('dotenv').config()
 
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
+const port = process.env.PORT
 
 let artist_id = '4Z8W4fKeB5YxbusRsdQVPb'
 
--- request_authorization_code.js
+++ request_authorization_code.js
@@ -1,5 +1,47 @@
 const axios = require('axios')
+const express = require('express')
 const querystring = require('querystring')
+
+exports.request_authorization = async function request_authorization(client_id, port) {
+  const app = express()
+
+  var state = generateRandomString(16)
+  var scope = 'user-read-private user-read-email'
+  const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
+    response_type: 'code',
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state
+  })
+  
+  
+  app.get('/', (req, res) => {
+    res.send('Não tem nada para você aqui')
+  })
+  
+  let code 
+  let state
+  
+  
+  app.get('/callback', (req, res) => {})
+  
+  await axios.get('http://localhost:8888/callback')
+    .then(reponse => {
+      console.log("Response:", response)
+      code = response.code
+      state = response.state
+    })
+    .catch(error => {
+      console.log('Error in request:', error)
+    })
+
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+  })
+}
+
+
 
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -12,16 +54,4 @@ function generateRandomString(length) {
 
   return result
 
-}
-
-async function request_authorization(client_id) {
-  var state = generateRandomString(16)
-  var scope = 'user-read-private user-read-email'
-  const login = "https://accounts.spotify.com/authorize?" + querystring.stringify({
-    response_type: 'code',
-    client_id: client_id,
-    scope: scope,
-    redirect_uri: redirect_uri,
-    state: state
-  })
 }
